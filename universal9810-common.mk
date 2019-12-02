6c5046762685e7e8585d49df5c1f8425b761b8b9
rohan <purohit.rohan@gmail.com>
2019-12-02 10:08:08 +0100

    universal9810: use Bliss Custom Doze

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -87,6 +87,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lineage.trust@1.0-service
 
+# Bliss Doze
+PRODUCT_PACKAGES += \
+    CustomDoze
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio_keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio_keys.kl \
